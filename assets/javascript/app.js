62903ae0b25f8b5ed1926d9fa3e70b24c86a53bd
Travis Pickering <Pickerings@Traviss-MacBook-Pro.local>
2016-12-07 01:14:25 -0600

    fixed event listener for pulling giphys and displaying on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,78 @@
 
 $('document').ready(function() {
-	console.log("beforeOnClick");
+	
 
 	var topics = ["christmas tree", "christmas gifts", "Griswalds", "wrapping paper", "christmas ornaments", "the elf", "eggnog"];
 
-	// for loop to load array topics into christmasButtons div
-	for (i=0; i <topics.length; i++) {
-		var topicWord = topics[i]; 
-		var newBtn = $("<button>").text(topicWord);
-		newBtn.addClass("arrayButton");
-		newBtn.appendTo($("#christmasButtons"));
-		console.log(topicWord);
+	function renderButton() {
+		// empty christmasButton container
+		$("#christmasButtons").empty();
+		// for loop to load array topics into christmasButtons div
+		for (i=0; i <topics.length; i++) {
+			var topicWord = topics[i]; 
+			var newBtn = $("<button>").text(topicWord);
+			newBtn.addClass("arrayButton");
+			newBtn.attr("data-word", topics[i]);
+			newBtn.appendTo($("#christmasButtons"));
+			console.log("this=", topicWord);
+		};
 	};
-
-	// on click to ajax pre-existing array words
-	$(document).on("click", "arrayButton", function() {
-		var christmasWord = ($(this).val());
-		console.log(christmasWord);
-	});
-
+	renderButton();
 
 
 	// on click to add new christmas words 
 	$("#giphySubmit").on("click", function() {
-	 		
+		 	
 			// create button for the favorite christmas input
 			var christmasWord = ($("#giphyWord").val().trim());
-			console.log(christmasWord);
-			console.log("working");
+			topics.push(christmasWord);
+			renderButton();
 
-	 		// create var for giphy api
-	       var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + christmasWord + "&api_key=dc6zaTOxFJmzC&limit=10";
-	 	
-	       //ajax object
-	       $.ajax({
-	         url: giphyURL,
-	         method: "GET"
-	       }).done(function(query) {
-	       		var results = query.data;
-	       		console.log(results);
-	       		// for loop to display all images in christmasButtons div
-
-	       });
-
-	   
+			console.log("line 30=",christmasWord);
 	});
+		 	
+
+	// on click 
+	$(document).on("click", ".arrayButton", function() {
+		
+		var christmasWord = $(this).data("word");
+		console.log("this=", christmasWord);
+		 	// create var for giphy api
+		    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + christmasWord + "&api_key=dc6zaTOxFJmzC&limit=10";
+		 	
+		    //ajax object
+		    $.ajax({
+		       url: giphyURL,
+		       method: "GET"
+		    }).done(function(query) {
+		    	console.log(query.data);
+			    var results = query.data;
+			        
+			    for (var i = 0; i < results.length; i++) {
+
+		            var giphDiv = $("<div class='item'>");
+
+		            var rating = results[i].rating;
+
+		            var p = $("<p>").text("Rating: " + rating);
+		            console.log(p);
+
+		            var christmasImage = $("<img>");
+		            christmasImage.attr("src", results[i].images.fixed_height.url);
+
+		            giphDiv.prepend(p);
+		            giphDiv.prepend(christmasImage);
+
+		            $("#giphyResults").prepend(giphDiv);
+
+	            	
+        		};
+
+		
+	
+			});
+
+		   
+	});
+
 });
